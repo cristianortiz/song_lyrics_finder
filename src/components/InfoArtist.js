018f4460e8859807eead57a3d6b9c8521ad6aeff
cristian <cristian.ortiz@uda.cl>
2021-04-05 22:10:10 -0400

    artist biography component and links to artist social media

--- src/components/InfoArtist.js
+++ src/components/InfoArtist.js
@@ -1,6 +1,7 @@
 import React from "react";
 const InfoArtist = ({ artist_info }) => {
   if (Object.keys(artist_info).length === 0) return null;
+  const { strArtistThumb, strGenre, strBiographyEN } = artist_info;
 
   return (
     <div className="card border-light">
@@ -8,7 +9,31 @@ const InfoArtist = ({ artist_info }) => {
         Artist Info
       </div>
       <div className="card-body">
-        <img src={artist_info.strArtistThumb} alt="Artist Logo"></img>
+        <img src={strArtistThumb} alt="Artist Logo"></img>
+        <p className="card-text">Genre: {strGenre}</p>
+        <h2 className="card-text">Biography:</h2>
+        <p className="card-text">{strBiographyEN}</p>
+        <a
+          href={`https://${artist_info.strFacebook}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a
+          href={`https://${artist_info.strTwitter}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a
+          href={`${artist_info.strLastFMChart}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-lastfm"></i>
+        </a>
       </div>
     </div>
   );
